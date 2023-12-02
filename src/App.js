5ee17d5c8b86c6c0e3cc51b2e662f8f4d8a5387e
mouni-at-944808117111 <likemesai@gmail.com>
2023-12-02 06:48:22 +0530

    all files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [file, setFile] = useState(null);
+
+ 
+
+  return (
+    <div className="App">
+      this is fresh react application.
+    </div>
+  );
+}
+
+export default App;
